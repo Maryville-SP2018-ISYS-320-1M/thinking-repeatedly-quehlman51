c215445f5c66ab828aa3fca43ae6c7cc68d5f554
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-05 00:43:36 -0500

    Input code and response

--- src/P8_Launch.java
+++ src/P8_Launch.java
@@ -12,7 +12,11 @@
 public class P8_Launch {
 
 	public static void main(String[] args) {
-
+		System.out.println("T-minus ");
+        for( int i = 5; i >= 1; i-- ) {
+            System.out.print( i + ", " );
+        }
+        System.out.println("Blastoff!");
 	}
 
 }
@@ -20,3 +24,4 @@ public class P8_Launch {
 
 
 // 3. Were you correct? Explain any differences and what you learned.
+//    For the most part I was. I missed the "println" on "T-minus "
