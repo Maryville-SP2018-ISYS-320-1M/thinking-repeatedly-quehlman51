c28044011397bcaf704bfcbddfc919dfe01fbf97
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-05 00:31:14 -0500

    Input code and response

--- src/P2_HowManyLines.java
+++ src/P2_HowManyLines.java
@@ -14,7 +14,9 @@
 public class P2_HowManyLines {
 
 	public static void main(String[] args) {
-
+		for( int i = 1; i <= 3; i++ ) 
+		    System.out.println( "How many lines" );
+		    System.out.println( "are printed?" );
 	}
 
 }
@@ -22,3 +24,6 @@ public class P2_HowManyLines {
 
 
 // 3. Were you correct? Explain any differences and what you learned.
+// 	  	I was incorrect. I did not see there was no curly bracket after the
+//      for loop, therefore it only considered the first line as part of 
+//      the loop and executed that line.
