23582553e351dc80a9003d732b215378b9941028
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-05 01:35:12 -0500

    Created for loops

--- src/P12_NumbersLeft.java
+++ src/P12_NumbersLeft.java
@@ -7,10 +7,7 @@
 public class P12_NumbersLeft {
 
 	public static void main(String[] args) {
-		for(int i = 1; i <=5; i++) {
-			for(int j = 1; j <= i; j++) 
-				System.out.print(i);
-				System.out.println(" ");
+		
 			
 		}
 	}
--- src/P13_NumbersRight.java
+++ src/P13_NumbersRight.java
@@ -1,13 +1,21 @@
 /*
   	ISYS 320
-  	Name(s):
+  	Name(s): Anthony Quehl
   	Date: 
 */
 
 public class P13_NumbersRight {
 
 	public static void main(String[] args) {
-
+		for(int i = 1; i <=5; i++) {
+			for(int j = 1; j <= (5 - i); j++) { 
+				System.out.print(" ");
+			}
+			for(int j = 1; j <= i; j++)
+			System.out.print(i);
+			System.out.println();
+		}
 	}
-
 }
+
+
