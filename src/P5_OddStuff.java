b14aeae6f32dcad83cceb2513db1de8909498302
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-05 00:35:19 -0500

    Input code and response

--- src/P5_OddStuff.java
+++ src/P5_OddStuff.java
@@ -13,7 +13,11 @@
 public class P5_OddStuff {
 
 	public static void main(String[] args) {
-
+		int number = 4;
+        for( int count = 1; count <= number; count++ ) {
+            System.out.println( number );
+            number = number / 2;
+        }
 	}
 
 }
@@ -21,3 +25,4 @@ public class P5_OddStuff {
 
 
 // 3. Were you correct? Explain any differences and what you learned.
+//    I was correct in my prediction
