4cd324b18c17f8d95116aa03f35fe25ce2133d48
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-05 00:04:06 -0500

    Inserted FOR loop info

--- src/P1_Count5.java
+++ src/P1_Count5.java
@@ -1,13 +1,13 @@
 /*
   	ISYS 320
-  	Name(s):
+  	Name(s): Anthony Quehl
   	Date: 
 */
 
 public class P1_Count5 {
     public static void main(String[] args) {
-        for( int i = /* FINISH ME */ ) {
-            System.out.println( /* FINISH ME */);
+        for(int i = 1; i <=5; i++) {
+            System.out.println("5 times " + i + " equals " + (i * 5));
         }
     }
 }
